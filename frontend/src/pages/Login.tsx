3b196d40788b1580ab2caf4cf738bb89c817055a
Piotr Zapiór <piotrzapior0@gmail.com>
2025-06-10 18:04:20 +0200

    Added: redirect uri and clientID

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 export default function Login() {
-    const clientId = "YOUR_CLIENT_ID"
-    const redirectUri = "https://your-frontend.onrender.com/callback"
+    const clientId = "25eea8a6e3bc40b4b0690602a17108a0"
+    const redirectUri = "https://music-game-frontend.onrender.com/callback"
     const scope = "streaming user-read-email user-read-private user-modify-playback-state"
 
     const loginUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`
