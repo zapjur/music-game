0a0c3aeebe9167372744cccbfe6008433f94d006
Piotr Zapiór <piotrzapior0@gmail.com>
2025-06-10 21:02:49 +0200

    fixing for mobile

--- spotify-proxy/handlers/play.go
+++ spotify-proxy/handlers/play.go
@@ -12,6 +12,10 @@ type PlayRequest struct {
 }
 
 func HandlePlay(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
 	var body PlayRequest
 	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
