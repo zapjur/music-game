bcc1b7cf60c3ec0c23929b2e33ee565087d16b08
Piotr Zapiór <piotrzapior0@gmail.com>
2025-06-10 19:30:25 +0200

    Added: CORS

--- spotify-proxy/main.go
+++ spotify-proxy/main.go
@@ -20,5 +20,17 @@ func main() {
 		port = "8080"
 	}
 	log.Println("Server running on port", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, withCORS(http.DefaultServeMux)))
+}
+func withCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "https://jurson.onrender.com")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
