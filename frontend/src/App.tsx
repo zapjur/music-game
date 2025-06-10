e161b6b7e1ce4fdb8d6b98c42ed40235d2435391
Piotr Zapiór <piotrzapior0@gmail.com>
2025-06-10 19:05:05 +0200

    fixing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
-import Game from "./pages/Game";
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Game from './pages/Game'
+import Callback from './pages/Callback'
 
-function App() {
-    return <Game />;
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/" element={<Game />} />
+            <Route path="/callback" element={<Callback />} />
+            <Route path="*" element={<div>404 – Nie znaleziono</div>} />
+        </Routes>
+    )
 }
-
-export default App;
