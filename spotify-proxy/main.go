c8d3ac26e08ad3d7f13d8cc9716cba5d10693ba6
Piotr Zapiór <piotrzapior0@gmail.com>
2025-06-10 21:05:27 +0200

    fixing for mobile

--- spotify-proxy/main.go
+++ spotify-proxy/main.go
@@ -27,7 +27,7 @@ func withCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "https://jurson.onrender.com")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
