2152587a319d5fd4e51b3c8fbd0ef32d512200ee
Piotr Zapiór <piotrzapior0@gmail.com>
2025-06-11 15:19:04 +0200

    fixing pilot redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
             <Route path="/" element={<Game />} />
             <Route path="/callback" element={<Callback />} />
             <Route path="/pilot" element={<Pilot />} />
-            <Route path="*" element={<div>404 – Nie znaleziono</div>} />
+            <Route path="*" element={<Pilot> /} />
         </Routes>
     )
 }
